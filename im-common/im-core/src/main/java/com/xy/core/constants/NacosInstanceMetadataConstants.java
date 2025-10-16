32da07af7011e0040a2f958eacb3d9770143e3ee
yj <xuyjun@foxmail.com>
2025-10-16 17:00:39 +0800

    feat: im-auth: return im-connect websocket endpoints in login response(#4)

---
+++ im-common/im-core/src/main/java/com/xy/core/constants/NacosInstanceMetadataConstants.java
@@ -0,0 +1,20 @@
+package com.xy.core.constants;
+
+public class NacosInstanceMetadataConstants {
+
+    private NacosInstanceMetadataConstants() {}
+
+    public static final String REGION= "region";
+
+    public static final String BROKER_ID= "brokerId";
+
+    public static final String VERSION= "version";
+
+    public static final String PROTOCOLS= "protocols";
+
+    public static final String PRIORITY= "priority";
+
+    public static final String CONNECTION= "connection";
+
+    public static final String CREATED_AT= "createdAt";
+}

7bec2f7a0ff8083b7bb783a307d9be5552322423
dennis9486 <382192293@qq.com>
2026-02-05 17:49:08 +0800

    refactor(knowledge): 移除AI搜索服务及相关配置
    - 删除AiChatResponse VO类
    - 删除AiSearchService接口及其实现类AiSearchServiceImpl
    - 删除application.yml中AI相关配置
    - 删除AuditLogPo实体类及AuditLogRepository接口
    - 删除CacheService接口及其实现类CacheServiceImpl
    - 删除数据库配置DatabaseConfig
    - 删除DocumentMapper接口及DocumentPo实体类
    - 删除DocumentRepository接口及DocumentService接口和实现类
    - 删除GroupRepository依赖相关代码

--- im-knowledge/src/main/java/com/xy/lucky/knowledge/es/mapper/EsKnowledgeDocMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xy.lucky.knowledge.es.mapper;
-
-import com.xy.lucky.knowledge.domain.es.EsKnowledgeDoc;
-import org.dromara.easyes.core.kernel.BaseEsMapper;
-
-public interface EsKnowledgeDocMapper extends BaseEsMapper<EsKnowledgeDoc> {
-}
--- im-knowledge/src/main/java/com/xy/lucky/knowledge/repository/DocumentVersionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xy.lucky.knowledge.repository;
-
-import com.xy.lucky.knowledge.domain.po.DocumentVersionPo;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface DocumentVersionRepository extends ReactiveCrudRepository<DocumentVersionPo, Long> {
-    Flux<DocumentVersionPo> findByDocumentId(Long documentId);
-}
